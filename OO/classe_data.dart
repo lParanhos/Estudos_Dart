a4e695093169c790b6f31d8d8878e402230ff437
lParanhos <leandro.paranhos@dextra-sw.com>
2020-08-12 20:46:44 -0300

    Dart OO - Class

---
+++ OO/classe_data.dart
@@ -0,0 +1,20 @@
+class Data {
+  int dia;
+  int mes;
+  int ano;
+}
+
+main() {
+  var dataAniversario = new Data();
+  dataAniversario.dia = 3;
+  dataAniversario.mes = 10;
+  dataAniversario.ano = 2020;
+
+  Data dataCompra = Data();
+  dataCompra.dia = 22;
+  dataCompra.mes = 10;
+  dataCompra.ano = 2020;
+
+  print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
+  print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
+}
