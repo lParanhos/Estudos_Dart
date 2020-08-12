6bc846ceccd81673543efec24e793624217bf889
lParanhos <leandro.paranhos@dextra-sw.com>
2020-08-12 20:58:42 -0300

    Dart OO - Methods

--- OO/classe_data.dart
+++ OO/classe_data.dart
@@ -2,6 +2,14 @@ class Data {
   int dia;
   int mes;
   int ano;
+
+  String obterFormatada() {
+    return "$dia/$mes/$ano";
+  }
+
+  String toString(){
+    return obterFormatada();
+  }
 }
 
 main() {
@@ -15,6 +23,17 @@ main() {
   dataCompra.mes = 10;
   dataCompra.ano = 2020;
 
-  print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
-  print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
+ // print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
+ // print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
+
+  String d1 = dataAniversario.obterFormatada();
+
+  print("A data do aniversário é $d1");
+  print("A data da compra é ${dataCompra.obterFormatada()}");
+
+  print(dataCompra);
+
+  Data d2 = dataCompra;
+  String s1 = dataCompra.toString();
+
 }
