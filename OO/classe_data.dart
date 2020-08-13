4ca89965c243d0d7bfefd92111c1642533d5f0fa
lParanhos <leandro.paranhos@dextra-sw.com>
2020-08-12 21:24:06 -0300

    Dart OO - Constructor

--- OO/classe_data.dart
+++ OO/classe_data.dart
@@ -3,6 +3,15 @@ class Data {
   int mes;
   int ano;
 
+/*   Data(int dia, int mes, int ano) {
+    this.dia = dia;
+    this.mes = dia;
+    this.ano = ano;
+  }
+ */
+
+  Data([this.dia = 1, this.mes = 1, this.ano = 1970]);
+
   String obterFormatada() {
     return "$dia/$mes/$ano";
   }
@@ -13,17 +22,13 @@ class Data {
 }
 
 main() {
-  var dataAniversario = new Data();
-  dataAniversario.dia = 3;
-  dataAniversario.mes = 10;
-  dataAniversario.ano = 2020;
+  var dataAniversario = new Data(3, 10, 2020);
 
-  Data dataCompra = Data();
+  Data dataCompra = Data(1, 1, 1970);
   dataCompra.dia = 22;
   dataCompra.mes = 10;
-  dataCompra.ano = 2020;
+  dataCompra.ano = 2020; 
 
- // print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
  // print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
 
   String d1 = dataAniversario.obterFormatada();
@@ -33,7 +38,9 @@ main() {
 
   print(dataCompra);
 
-  Data d2 = dataCompra;
-  String s1 = dataCompra.toString();
+  print(new Data());
+  print(Data(12));
+  print(Data(12, 11));
+  print(Data(12, 11, 2020));
 
 }
